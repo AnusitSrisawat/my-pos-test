5cebb6430ce4a03302d7a847616dae1f06175f0d
Jimmy <jimmy18778@gmail.com>
2024-04-07 01:34:23 +0700

    fix route

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
 import '/src/app/globals.css'
- 
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { isUserAuthenticated } from '../pages/api/auth';
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const router = useRouter();
+
+  useEffect(() => {
+    // Check authentication status when component mounts
+    const isAuthenticated = isUserAuthenticated();
+
+    // Redirect to login page if user is not authenticated
+    if (!isAuthenticated && router.pathname !== '/login') {
+      router.push('/login');
+    }
+  }, []);
+
+  return (
+    <>
+      {router.pathname === '/login' ? (
+        <LoginPage />
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
+  )
+
 }
