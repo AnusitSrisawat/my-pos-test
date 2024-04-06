596a406a83bfda13a10172ec88ff455e0d8eb2cc
Jimmy <jimmy18778@gmail.com>
2024-04-07 03:05:19 +0700

    Skeleton test

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useEffect, useState } from 'react';
-
+import { Skeleton } from "@nextui-org/react";
 interface Item {
     id: number;
     name: string;
@@ -16,6 +16,7 @@ interface ProductProps {
 const Product: React.FC<ProductProps> = ({ items, onAddData }) => {
 
     const [product, setProduct] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
         // Function to call when component starts
@@ -27,6 +28,7 @@ const Product: React.FC<ProductProps> = ({ items, onAddData }) => {
         const response = await fetch('/api/product').then(res => res.json());
         // console.log("api/product", response);
         setProduct(response);
+        setIsLoaded(true);
     };
 
 
@@ -40,10 +42,11 @@ const Product: React.FC<ProductProps> = ({ items, onAddData }) => {
                     <div key={item.id}
                         onClick={() => onAddData(item)}
                         className='cursor-pointer bg-slate-100 bg-opacity-50 relative flex flex-col gap-2 p-2 border-2 border-transparent hover:border-2 hover:border-black rounded-xl hover:scale-105 duration-200'>
-                        <div className='relative w-full h-44 object-cover rounded-xl'>
-                            <img className='w-full h-full object-cover rounded-xl' src={item.image} alt={item.name} />
+                        <Skeleton isLoaded={isLoaded} className="w-full h-44">
+                            <div className='relative w-full h-44 object-cover rounded-xl'>
+                                <img className='w-full h-full object-cover rounded-xl' src={item.image} alt={item.name} />
 
-                            {/* {true ? (
+                                {/* {true ? (
                         <span></span>
                     ) : (
                         <div className='absolute top-0 left-0 w-full p-2 flex flex-row gap-2 justify-end'>
@@ -51,18 +54,19 @@ const Product: React.FC<ProductProps> = ({ items, onAddData }) => {
                         </div>
                     )} */}
 
-                            {items.map((items: any) => {
-                                if (items.id === item.id && items.qty > 0) {
-                                    return (
-                                        <div key={items.id} className='absolute top-0 right-0 bg-slate-100 font-medium rounded-lg p-1 m-1 w-6 h-6 flex justify-center items-center '>
-                                            {items.qty}
-                                        </div>
-                                    );
-                                } else {
-                                    return null; // Return null if condition is not met
-                                }
-                            })}
-                        </div>
+                                {items.map((items: any) => {
+                                    if (items.id === item.id && items.qty > 0) {
+                                        return (
+                                            <div key={items.id} className='absolute top-0 right-0 bg-slate-100 font-medium rounded-lg p-1 m-1 w-6 h-6 flex justify-center items-center '>
+                                                {items.qty}
+                                            </div>
+                                        );
+                                    } else {
+                                        return null; // Return null if condition is not met
+                                    }
+                                })}
+                            </div>
+                        </Skeleton>
                         <div className=''>
                             <div className='flex flex-row justify-between w-full'>
                                 <div className="">{item.name}</div>
