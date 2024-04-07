bcd19d0eaf9ff104153e5a95a22f9f8d40552a51
Jimmy <jimmy18778@gmail.com>
2024-04-07 13:54:51 +0700

    dropdown salepage

--- pages/SalePage.tsx
+++ pages/SalePage.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import Cal from '../src/components/Cal';
 import Product from '../src/components/Product';
+import { Dropdown, DropdownTrigger, Button, DropdownMenu, DropdownItem, User, Avatar } from '@nextui-org/react';
 
 interface Item {
     id: number;
@@ -124,24 +125,72 @@ export default function SalePage() {
     return (
         <div className='h-screen w-full flex flex-col overflow-hidden'>
             <div className={'relative w-full bg-slate-800 text-slate-300 flex flex-row justify-between items-center px-4 duration-500' + (showNav ? ' h-[10%] py-2' : ' h-0')}>
+                
                 <div className='flex flex-row gap-8'>
-                    <div className=''>
-                        dashboad
-                    </div>
-                    <div className=''>
-                        dashboad
-                    </div>
-                    <div className=''>
-                        dashboad
-                    </div>
+                    <Dropdown className=''>
+                        <DropdownTrigger>
+                            <Button className='text-white'
+                                variant="light"
+                            >
+                                Product
+                            </Button>
+                        </DropdownTrigger>
+                        <DropdownMenu aria-label="Static Actions">
+                            <DropdownItem key="new">New file</DropdownItem>
+                            <DropdownItem key="copy">Copy link</DropdownItem>
+                            <DropdownItem key="edit">Edit file</DropdownItem>
+                            <DropdownItem key="delete" className="text-danger" color="danger">
+                                Delete file
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
                 </div>
-                <div className='flex flex-row justify-center items-center gap-4'>
+
+                {/* <div className='flex flex-row justify-center items-center gap-4'>
                     <div className=''>
                         Anusit Srisawat
                     </div>
                     <div className='rounded-full overflow-hidden bg-black h-16 w-16 shadow-2xl'>
                         <img className='w-full h-full object-cover' src="https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTExL3BmLXMxMDgtcG0tNDExMy1tb2NrdXAuanBn.jpg" alt="" />
                     </div>
+                </div> */}
+
+                <div className="flex items-center gap-4">
+                    <Dropdown placement="bottom-start">
+                        <DropdownTrigger>
+                            <User
+                                as="button"
+                                avatarProps={{
+                                    isBordered: false,
+                                    src: "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTExL3BmLXMxMDgtcG0tNDExMy1tb2NrdXAuanBn.jpg",
+                                }}
+                                className="transition-transform"
+                                description="@tonyreichert"
+                                name="Tony Reichert"
+                            />
+                        </DropdownTrigger>
+                        <DropdownMenu aria-label="User Actions" variant="flat">
+                            <DropdownItem key="profile" className="h-14 gap-2">
+                                <p className="font-bold">Signed in as</p>
+                                <p className="font-bold">@tonyreichert</p>
+                            </DropdownItem>
+                            <DropdownItem key="settings">
+                                My Settings
+                            </DropdownItem>
+                            <DropdownItem key="team_settings">Team Settings</DropdownItem>
+                            <DropdownItem key="analytics">
+                                Analytics
+                            </DropdownItem>
+                            <DropdownItem key="system">System</DropdownItem>
+                            <DropdownItem key="configurations">Configurations</DropdownItem>
+                            <DropdownItem key="help_and_feedback">
+                                Help & Feedback
+                            </DropdownItem>
+                            <DropdownItem key="logout" color="danger">
+                                Log Out
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
                 </div>
                 <div className={'absolute h-5 flex justify-center items-start bottom-0 left-1/2 bg-slate-700 px-2 z-10 cursor-pointer ' + (showNav ? ' translate-y-0 rounded-t-lg leading-[1.1]' : ' translate-y-full rounded-b-lg leading-[0.8]')} onClick={() => { setShowNav(!showNav) }}>
                     {showNav ? 'close' : 'open'}
